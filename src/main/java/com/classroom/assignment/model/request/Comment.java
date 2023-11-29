4d74cb8e697170da9c3bcb8bd8d2403f79e5fc03
23011112 <150985738+23011112@users.noreply.github.com>
2023-11-29 06:02:30 +0000

    STEP3入力フォームを作ってみよう

--- src/main/java/com/classroom/assignment/model/request/Comment.java
+++ src/main/java/com/classroom/assignment/model/request/Comment.java
@@ -1,13 +1,14 @@
 package com.classroom.assignment.model.request;
-
 public class Comment {
 
   private String name;
   private String content;
+  private String mail;
 
-  public Comment(String name, String content) {
+  public Comment(String name, String content, String mail) {
     this.setName(name);
     this.setContent(content);
+    this.setMail(mail);
   }
 
   public String getName() {
@@ -24,5 +25,13 @@ public class Comment {
 
   public void setContent(String content) {
     this.content = content;
+  }
+
+  public String getMail() {
+    return mail;
+  }
+
+  public void setMail(String mail) {
+    this.mail = mail;
   }
 }
